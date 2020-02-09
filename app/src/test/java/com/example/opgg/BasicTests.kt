4554fc80d805f0bbbbf14c5ba34156d7de803737
yesjin <eungjin@qualson.com>
2020-02-10 02:11:00 +0900

    feat: Add simple passed time checker and test

--- app/src/test/java/com/example/opgg/BasicTests.kt
+++ app/src/test/java/com/example/opgg/BasicTests.kt
@@ -3,6 +3,7 @@ package com.example.opgg
 import android.util.Log
 import com.example.opgg.common.BASE_URL
 import com.example.opgg.common.roundUp
+import com.example.opgg.common.timePassed
 import com.example.opgg.data.summoner.SummonerService
 import com.example.opgg.di.ApiModule
 import com.google.gson.Gson
@@ -25,15 +26,39 @@ import kotlin.math.roundToInt
 class BasicTests {
     @Test
     fun addition_isCorrect() {
+        print(System.currentTimeMillis() - 1571654629000)
         assertEquals(4, 2 + 2)
     }
 
     @Test
     fun roundup_works_well() {
-        val tester = 2.336
-        val res = tester.roundUp(2)
-        print(res)
-        assertEquals(2.34, res, 0.toDouble())
+
+        val tester1 = 2.36
+        val res1 = tester1.roundUp()
+
+        val tester2 = 2.336
+        val res2 = tester2.roundUp(2)
+
+        assertEquals(2.4, res1, 0.toDouble())
+        assertEquals(2.34, res2, 0.toDouble())
+    }
+
+    @Test
+    fun time_passed_checker_works_well() {
+        val currentTime = System.currentTimeMillis() / 1000
+
+        val res1 = timePassed(currentTime - 50)
+        assertEquals("1분 전", res1)
+
+
+        val res2 = timePassed(currentTime - 130)
+        assertEquals("2분 전", res2)
+
+        val res3 = timePassed(currentTime - 3610)
+        assertEquals("1시간 전", res3)
+
+        val res4 = timePassed(currentTime - (2 * 60 * 60 * 24 + 10))
+        assertEquals("2일 전", res4)
     }
 
 }
