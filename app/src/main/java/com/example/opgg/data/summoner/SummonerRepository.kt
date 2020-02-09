759e903851f5e5915d59c846630fe4549be03057
yesjin <eungjin@qualson.com>
2020-02-09 23:28:52 +0900

    feat: Add summoner api request methods

--- app/src/main/java/com/example/opgg/data/summoner/SummonerRepository.kt
+++ app/src/main/java/com/example/opgg/data/summoner/SummonerRepository.kt
@@ -6,10 +6,19 @@ import javax.inject.Inject
  * Data repository for summoner related data.
  */
 class DefaultSummonerRepository @Inject constructor(
-    remoteSource: SummonerDataSource
+    private val remoteSource: SummonerDataSource
 ) : SummonerRepository {
 
+    override suspend fun getGenetory(): Genetory {
+        return remoteSource.getGenetory()
+    }
+
+    override suspend fun getMatches(lastMatch: Int): MatchData.Match {
+        return remoteSource.getMatches(lastMatch)
+    }
 }
 
 interface SummonerRepository {
+    suspend fun getGenetory(): Genetory
+    suspend fun getMatches(lastMatch: Int): MatchData.Match
 }
